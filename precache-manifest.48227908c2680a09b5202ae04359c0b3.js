b4cf2ac134c16a97e0f45f49b63426f8029e099a
Samuel Sepeši <samuel.sepesi@student.tuke.sk>
2019-07-13 16:45:57 +0200

    Updates

---
+++ precache-manifest.48227908c2680a09b5202ae04359c0b3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccd8c2f60f9c7ed7e9a8c46c90a1729c",
+    "url": "/trips-app-frontend/index.html"
+  },
+  {
+    "revision": "234bd9e92ebf53fd3ead",
+    "url": "/trips-app-frontend/static/js/2.fb5bed30.chunk.js"
+  },
+  {
+    "revision": "e1d730cc0b3bf92f540d",
+    "url": "/trips-app-frontend/static/js/main.c12ca23d.chunk.js"
+  },
+  {
+    "revision": "192d2baf0ca15ff71667",
+    "url": "/trips-app-frontend/static/js/runtime~main.dd7fea44.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trips-app-frontend/precache-manifest.586d529fa69f8c3f5a6eb62d6dab4b39.js"
+  "/trips-app-frontend/precache-manifest.48227908c2680a09b5202ae04359c0b3.js"
 );
 
 self.addEventListener('message', (event) => {
